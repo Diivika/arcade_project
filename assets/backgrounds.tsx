65e4e07e6dba8cbc9007e4f352403c9d44c387ee
Diivika <vasdima4466@gmail.com>
2026-01-09 14:58:10 +0300

    fixed

--- assets/backgrounds.tsx
+++ assets/backgrounds.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="backgrounds" tilewidth="18" tileheight="18" spacing="2" tilecount="27" columns="9">
- <image source="assets/Tilemap/tilemap-backgrounds_packed.png" width="192" height="72"/>
+ <image source="Tilemap/tilemap-backgrounds_packed.png" width="192" height="72"/>
 </tileset>
